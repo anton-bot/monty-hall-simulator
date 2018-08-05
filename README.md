6f75101bed187d21e1e9ba867f693e3066b57695
Anton Ivanov <anton@ivanov.hk>
2018-08-05 16:24:01 +0800

    commit initial version

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# monty-hall-simulator
+# Monty Hall Problem Simulator
+
 Simulates the Monty Hall Problem over a given number of iterations, with an arbitrary number of doors.
+
+Uses a cryptographically secure pseudorandom number generator. Returns the results or prints them to console.
+
+For a more convincing demonstration, you can play a game with 100 doors, when Monty opens 98 doors before offering to switch. This results in a 99% winning chance when switching.
+
+## async monty(iterations, doors, print)
+
+- `iterations` - Number of games for each scenario (player switches / player stays). Default 1000.
+- `doors` - Number of doors in the game. Default 3.
+- `print` - Whether to print results to console (default: `true`). If false, returns results in the format:
+
+```json
+{
+  "iterations": 1000,
+  "wins": {
+    "kept": 333,
+	"switched": 667,
+  }
+}
+```
+
+## Examples
+
+### Classical Monty Hall game: 
+
+```js
+
+const monty = require('monty-hall-simulator');
+
+monty();
+
+//  ---------------------------------
+//  |                               |
+//  |  Monty Hall Problem Simulator |
+//  |                               |
+//  ---------------------------------
+//
+//  Played 1000 iterations
+//  for each game scenario.
+//
+//  Wins when player keeps the initial bet: 354/1000 (35.40%)
+//  Wins when player switches their bet:    657/1000 (65.70%)
+
+```
+
+### Play more iterations - to get more precise results
+
+```js
+const monty = require('monty-hall-simulator');
+
+monty(1000000);
+
+//  ---------------------------------
+//  |                               |
+//  |  Monty Hall Problem Simulator |
+//  |                               |
+//  ---------------------------------
+//
+//  Played 1000000 iterations
+//  for each game scenario.
+//
+//  Wins when player keeps the initial bet: 334154/1000000 (33.42%)
+//  Wins when player switches their bet:    666065/1000000 (66.61%)
+```
+
+### Play with more doors 
+
+```js
+const monty = require('monty-hall-simulator');
+
+monty(10000, 100);
+
+//  ---------------------------------
+//  |                               |
+//  |  Monty Hall Problem Simulator |
+//  |                               |
+//  ---------------------------------
+//
+//  Played 10000 iterations
+//  for each game scenario.
+//
+//  Wins when player keeps the initial bet: 97/10000 (0.97%)
+//  Wins when player switches their bet:    9898/10000 (98.98%)
+```
---
+++ index.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const random = require('random-number-csprng');
+
+const CHOICES = {
+  KEEP: false,
+  SWITCH: true,
+};
+
+const PRIZES = {
+  GOAT: false,
+  CAR: true,
+};
+
+async function montyHall(iterations = 1000, doors = 3, print = true) {
+  if (doors < 3) {
+    throw new Error('The number of doors must be at least 3.');
+  }
+
+  const wins = {
+    kept: 0, // player insisted on their initial bet
+    switched: 0, // player switched their bet
+  };
+
+  let i = 0;
+  while (i < iterations) {
+    wins.kept += await play(CHOICES.KEEP, doors);
+    wins.switched += await play(CHOICES.SWITCH, doors);
+    i++;
+  }
+
+  if (print) {
+    console.log(`
+  ---------------------------------
+  |                               |
+  |  Monty Hall Problem Simulator |
+  |                               |
+  ---------------------------------
+
+  Played ${iterations} iterations
+  for each game scenario.
+
+  Wins when player keeps the initial bet: ${formatWins(wins.kept, iterations)}
+  Wins when player switches their bet:    ${formatWins(wins.switched, iterations)}`);
+  } else {
+    return { iterations, wins };
+  }
+}
+
+/** Prints the results as "670 (67.00%)". */
+function formatWins(wins, iterations) {
+  return `${wins}/${iterations} (${((wins / iterations) * 100).toFixed(2)}%)`;
+}
+
+/** Executes one iteration of the game. */
+async function play(playerSwitches, numDoors) {
+  // Initialize the doors with goats, place a car randomly:
+  let doors = new Array(numDoors);
+  doors.fill(PRIZES.GOAT);
+  doors[await random(0, numDoors - 1)] = PRIZES.CAR;
+
+  // Player makes a bet:
+  let playerBet = await random(0, numDoors - 1);
+
+  // Monty opens a door with a goat - we eliminate it from the array:
+  while (true) {
+    let goatCandidateDoor = await random(0, numDoors - 1);
+    if (goatCandidateDoor !== playerBet && doors[goatCandidateDoor] === PRIZES.GOAT) {
+      doors.splice(goatCandidateDoor, 1);
+
+      // Account for the shifted array indices after removing one element:
+      if (playerBet > goatCandidateDoor) {
+        playerBet--;
+      }
+
+      break;
+    }
+  }
+
+  if (playerSwitches) {
+    while (true) {
+      let newRandomBet = await random(0, doors.length - 1);
+
+      // Do not allow new bet to be the same as the previous one:
+      if (newRandomBet !== playerBet) {
+        playerBet = newRandomBet;
+        break;
+      }
+    }
+  }
+
+  // True if car:
+  return doors[playerBet] ? 1 : 0;
+}
+
+module.exports = montyHall;
